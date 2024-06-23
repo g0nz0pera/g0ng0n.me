958871008abe8d25e57110e5f5acd5a5f5f51adf
G0ng0n <155404796+g0nz0pera@users.noreply.github.com>
2024-06-23 17:12:51 -0300

    fiinishing the hero part of the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavBar";
+import { IconBriefcase, IconHome, IconMail } from "@tabler/icons-react";
 
 export default function Home() {
   return (
-    <div className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-
+        <FloatingNav
+          navItems={[
+            { name: "Home", link: "/", icon: <IconHome /> },
+            { name: "Projects", link: "/", icon: <IconBriefcase /> },
+            { name: "Contact Me", link: "/", icon: <IconMail /> },
+          ]}
+        />
+        <Hero />
       </div>
     </div>
   );
